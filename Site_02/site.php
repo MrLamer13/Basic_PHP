b32186106217ee26e25e6949413498727d3015ee
MrLamer <mrlamer@mail.ru>
2022-12-05 18:00:11 +0700

    Практическое задание №1

---
+++ Site_02/site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?=$title?></title>
+	<meta charset="UTF-8">
+</head>
+<body>
+<h1><?=$header?></h1>
+<?=$info?>
+<br><br>
+<img src="<?=$img?>" alt="photo">
+<br><br>
+<br><br>
+<b><?=$fatText?></b>
+<br><br>
+<?=$year?>
+</body>
+</html>
